8419a30256b3a3c1a8b6722ce0a632831cd0be76
Jaret Wright <hkkA223>
2020-01-20 14:59:06 -0500

    Added the view, controller and start method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args)
     {
         //Create a DeckOfCards
@@ -24,5 +30,23 @@ public class Main {
         //       the deck is empty
 
         //Here in the main method, deal 2 hands of Card objects.  5 Card objects per hand.
+        ArrayList<Card> p1Hand = new ArrayList<>();
+        ArrayList<Card> p2Hand = new ArrayList<>();
+
+        //deal 5 Card objects to each player
+        for (int i=1; i<=5;i++)
+        {
+            p1Hand.add(deck.dealTopCard());
+            p2Hand.add(deck.dealTopCard());
+        }
+
+        //display the contents of each players' hand and then we will take a break
+        System.out.printf("Player 1's hand: %s%n",p1Hand);
+        System.out.printf("Player 2's hand: %s%n",p2Hand);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("CardView.fxml"));
     }
 }
